c0419cedf940f323bd3b81f6118441c7de4ed6fe
16max <schelemax@g16hh.de>
2015-11-06 08:24:22 +0100

    06.11.2015

--- auf/01_bash.sh
+++ auf/01_bash.sh
@@ -6,11 +6,9 @@ cd 01/
 # Schreiben Sie einen Text in die Datei und veranlassen Sie das Skript, diesen Text anschließend auf der Konsole auszugeben.
 # echo "Das ist ein Test" >> Neue\ Datei
 echo "Das ist ein Test" > Neue\ Datei
-echo `cat Neue\ Datei`
-#echo `less Neue\ Datei`
+echo `cat Neue\ Datei` #==	echo `less Neue\ Datei`
 # Lassen Sie folgende Infos auf der Konsole ausgeben: wer ist eingeloggt, Name des Benutzers, aktuelles Verzeichnis, Home Verzeichnis.
 echo `who`
 echo `whoami`
 echo `pwd`
-#echo `echo ~`
-echo $HOME
+echo `echo ~` #==	echo $HOME
